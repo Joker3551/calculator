f8219904774fb2a4f01d3a13c7446d2ec879ce06
Joker3551 <machinima96@mail.ru>
2024-09-22 15:14:22 +1000

    Изменены взаимодействия римских  цифр с операторами

--- calculator.go
+++ calculator.go
@@ -24,17 +24,26 @@ func main() {
 	}
 
 	ArabToRome := map[int]string{
-		0:  "Вечная пустота",
-		1:  "I",
-		2:  "II",
-		3:  "III",
-		4:  "IV",
-		5:  "V",
-		6:  "VI",
-		7:  "VII",
-		8:  "VIII",
-		9:  "IX",
-		10: "X",
+		0:   " ",
+		1:   "I",
+		2:   "II",
+		3:   "III",
+		4:   "IV",
+		5:   "V",
+		6:   "VI",
+		7:   "VII",
+		8:   "VIII",
+		9:   "IX",
+		10:  "X",
+		20:  "XX",
+		30:  "XXX",
+		40:  "XL",
+		50:  "L",
+		60:  "LX",
+		70:  "LXX",
+		80:  "LXXX",
+		90:  "XC",
+		100: "C",
 	}
 
 	reader := bufio.NewReader(os.Stdin)
@@ -96,16 +105,16 @@ func main() {
 	}
 
 	if rome {
+		div := res / 10
+		mod := res % 10
 		if res < 0 {
 			panic("В Римской системе нет отрицательных чисел!")
-		} else if res > 10 {
-			if o1 > o2 {
-				fmt.Println(ArabToRome[o1] + ArabToRome[o2])
+		} else if ArabToRome[div*10] != "" {
+			if res < 10 {
+				fmt.Println(ArabToRome[res])
 			} else {
-				fmt.Println(ArabToRome[o2] + ArabToRome[o1])
+				fmt.Println(ArabToRome[div*10] + ArabToRome[mod])
 			}
-		} else {
-			fmt.Println(ArabToRome[res])
 		}
 	} else {
 		fmt.Println(res)
